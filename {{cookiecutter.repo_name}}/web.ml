794c9742c8a9cb463f36943b0289fa3b91c4d9ac
Etienne Millon <me@emillon.org>
2014-04-24 15:03:32 +0200

    game -> main

--- {{cookiecutter.repo_name}}/web.ml
+++ {{cookiecutter.repo_name}}/web.ml
@@ -1,9 +1,9 @@
 let js s = Js.string s
 
 let _ =
-  let game = 
+  let main =
     Js.Opt.get
       (Dom_html.document##getElementById (js"main"))
       (fun () -> assert false)
   in
-  game##innerHTML <- js"Hello, world!"
+  main##innerHTML <- js"Hello, world!"
